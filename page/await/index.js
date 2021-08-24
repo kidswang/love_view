82fecdaedeef01bba29bc6c82e7eb3fe43974d28
mhwangzl <705016627@qq.com>
2021-08-24 16:43:21 +0800

    提示

--- page/await/index.js
+++ page/await/index.js
@@ -11,12 +11,12 @@ Page({
       var gap = list[i].id - totalDay
       list[i].gap = Math.abs(gap)
       if (gap >= 0) {
-        list[i].pre = "距离恋爱"
+        list[i].pre = "距离"
         list[i].center = "还有"
         list[i].end = "天"
         list[i].finish = false
       } else {
-        list[i].pre = "恋爱"
+        list[i].pre = "XX"
         list[i].center = ""
         list[i].end = " ✓"
         list[i].finish = true
@@ -35,6 +35,7 @@ Page({
     }
   },
 
+
   data: {
     totalDay: 0,
     totalMarryDay: 0,
@@ -42,4 +43,29 @@ Page({
       { id: 1, desc: "99天" }
     ]
   },
+
+  clickTap(id) {
+    console.log(id);
+
+  },
+
+  deleteTap(id) {
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除此图片吗？',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('点击确定了');
+          images.splice(index, 1);
+        } else if (res.cancel) {
+          console.log('点击取消了');
+          return false;
+        }
+        that.setData({
+          images
+        });
+      }
+    })
+  },
+
 })
---
+++ util/request/FileUtil.js
@@ -0,0 +1,12 @@
+const baseUrl = "47.102.100.160:8080"
+
+/**
+ * get 请求方式
+ * @param url {String} 接口地址
+ * @param params {Object} 接口参数
+ * @returns {AxiosPromise}
+ * @constructor
+ */
+ export function getBaseUrl() {
+    return baseUrl;
+  }
